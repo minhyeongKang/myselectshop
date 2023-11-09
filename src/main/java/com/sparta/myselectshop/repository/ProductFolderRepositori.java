7ab6a619811429f2046ad65a624f5f73265c9f8a
minhyeongKang <kang7937@gmail.com>
2023-11-09 19:44:48 +0900

    관심 상품에 폴더 추가 및 폴더 전체 구현

--- src/main/java/com/sparta/myselectshop/repository/ProductFolderRepositori.java
+++
@@ -1,8 +0,0 @@
-package com.sparta.myselectshop.repository;
-
-import com.sparta.myselectshop.entity.ProductFolder;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductFolderRepositori extends JpaRepository<ProductFolder, Long> {
-
-}
