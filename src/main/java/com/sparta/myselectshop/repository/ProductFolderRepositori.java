dc53a5fa3c985f73bc8439fea471db0573c150e4
minhyeongKang <kang7937@gmail.com>
2023-11-09 17:09:53 +0900

    상품 폴더 설계

---
+++ src/main/java/com/sparta/myselectshop/repository/ProductFolderRepositori.java
@@ -0,0 +1,8 @@
+package com.sparta.myselectshop.repository;
+
+import com.sparta.myselectshop.entity.ProductFolder;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductFolderRepositori extends JpaRepository<ProductFolder, Long> {
+
+}
